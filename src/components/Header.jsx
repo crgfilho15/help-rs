98b141a40da50ca724b4a269b57605f5fe56cfb5
Carlos Roberto <crgfilho15@gmail.com>
2024-05-05 14:13:20 -0300

    Atualizando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import styles from './Header.module.css';
+import github from '../img/github.png';
+import linkedin from '../img/linkedin.png';
 
 export function Header() {
     return (
@@ -9,10 +11,10 @@ export function Header() {
             </div>
             <div>
                 <a href="https://github.com/crgfilho15/help-rs">
-                    <img className={styles.logo} src="src\img\github.png" alt="Logotipo GitHub" />
+                    <img className={styles.logo} src={github} alt="Logotipo GitHub" />
                 </a>
                 <a href="https://www.linkedin.com/in/carlosrobertogarcia/">
-                    <img className={styles.logo} src="src\img\linkedin.png" alt="Logotipo LinkedIn" />
+                    <img className={styles.logo} src={linkedin} alt="Logotipo LinkedIn" />
                 </a>
             </div>
         </nav>
